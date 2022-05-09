09e40f5144b8bcd02149e793317ed788658729d6
Isis Wyche <iwyche1@jhmi.edu>
2022-05-09 10:21:50 -0400

    Update multiple_individuals_labeling_toolbox.py

--- deeplabcut/gui/multiple_individuals_labeling_toolbox.py
+++ deeplabcut/gui/multiple_individuals_labeling_toolbox.py
@@ -160,7 +160,7 @@ class ImagePanel(BasePanel):
                 cIdx = cIdx / 255
                 color = cmap(cIdx, bytes=True)[:-1]
                 color = tuple([int(x) for x in color])
-                drawImage = cv2.line(drawImage, (x0, y0), (x1, y1), color, 1)
+                drawImage = cv2.line(drawImage, (x0, y0), (x1, y1), color, 1, lineType=cv2.LINE_AA)
         return drawImage
 
     def drawplot(self, img, img_name, itr, index, bodyparts, cmap, keep_view=False):
